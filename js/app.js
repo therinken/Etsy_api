e0e01263237fd9003b3de854d54e48759ea031f3
Ken McLeod <admin@therinken.com>
2014-10-21 18:24:24 -0500

    changes to include routing and template fix

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 window.onload = app;
 
+// runs when the DOM is loaded
+
 function app() {
 
     // load some scripts (uses promises :D)
@@ -26,73 +28,98 @@ function EtsyClient(options) {
         throw new Error("Yo dawg, I heard you like APIs. Y U NO APIKEY!?!?");
     }
     this.etsy_url = "https://openapi.etsy.com/";
-    this.version = options.api_version || "v2/";
+    this.version = options.api_version || "v2/"; // handle api version... if not given, just use the default "v2"
     this.api_key = options.api_key;
     this.complete_api_url = this.etsy_url + this.version;
 
-    this.init();
-};
+    // derp.
+    this.setupRouting();
+}
 
 EtsyClient.prototype.pullAllActiveListings = function() {
-    var model = 'listings';
-    var filter = 'active';
-    return $.getJSON(this.complete_api_url + model + '/' + filter + ".js?api_key=" + this.api_key + "&includesImages&callback=?").then(function(data) {
-        console.log(data);
+    return $.getJSON(
+        this.complete_api_url + "listings/active.js?api_key=" + this.api_key + "&includes=Images&callback=?"
+    )
+        .then(function(data) {
+            return data;
+        });
+}
+
+EtsyClient.prototype.pullSingleListing = function(id) {
+    return $.getJSON(this.complete_api_url + "listings/"+id+".js?api_key=" + this.api_key + "&includes=Images&callback=?").then(function(data) {
+        return data;
     });
-};
-
-EtsyClient.prototype.getListingInfo = function(id) {
-    var model = 'listings';
-    return $.getJSON(this.complete_api_url + model + '/' + id + ".js?api_key=" + this.api_key + "&includesImages&callback=?").then(function(data) {
-        console.log(data);
-    })
 }
 
-EtsyClient.prototype.getUserInfo = function() {
-    return $.getJSON(this.complete_api_url + model + '/' + id + ".js?api_key=" + this.api_key + "&includesImages&callback=?").then(function(data) {
-        console.log(data);
-    })
-}
+EtsyClient.prototype.loadTemplate = function(name) {
+    if (!this.templates) {
+        this.templates = {};
+    }
 
-EtsyClient.prototype.loadTemplateFile = function(templateName){
-    return $.get('./templates/' + templateName + '.html').then(function(htmlstring) {
-        return htmlstring;
-    })
-}
-
-EtsyClient.prototype.putProfileDataOnPage = function(profileHtml, profile) {
-    //var grid = document.querySelector("#listings")
-    document.querySelector('').innerHTML = _.template(profileHtml, profile);
-}
-
-EtsyClient.prototype.putListingDataOnPage = function(listingHtml, listing) {
-    document.querySelector('').innerHTML = _.template(listingHtml, listing);
-}
-
-EtsyClient.prototype.putCartDataOnPage = function(cartHtml, cart) {
-    document.querySelector('').innerHTML = _.template(cartHtml, cart);
-};
-
-EtsyClient.prototype.init = function() {
     var self = this;
 
-    $.when(
-        this.pullAllActiveListings(),
-        this.getListingInfo(),
-        this.loadTemplateFile('profile'),
-        this.loadTemplateFile('listing'),
-        this.loadTemplateFile('cart')
-        ).then(function(profile, listing, cart, profileHtml, listingHtml, cartHtml) {
-            self.putProfileDataOnPage(profileHtml, profile)
-            self.putListingDataOnPage(listingHtml, listing)
-            self.putCartDataOnPage(cartHtml, cart)
-        })
-};
-
-function app(){
-    var myEtsy = new EtsyClient({api_key: "aavnvygu0h5r52qes74x9zvo"});
-    myEtsy.pullAllActiveListings();
+    if (this.templates[name]) {
+        var promise = $.Deferred();
+        promise.resolve(this.templates[name]);
+        return promise;
+    } else {
+        return $.get('./templates/' + name + '.html').then(function(data) {
+            self.templates[name] = data; // <-- cache it for any subsequent requests to this template
+            return data;
+        });
+    }
 }
+
+EtsyClient.prototype.drawListings = function(templateString, data) {
+    var grid = document.querySelector("#listings");
+
+    var bigHtmlString = data.results.map(function(listing) {
+        return _.template(templateString, listing);
+    }).join('');
+
+    grid.innerHTML = bigHtmlString;
+}
+
+EtsyClient.prototype.drawSingleListing = function(template, data) {
+    var listing = data.results[0];
+    var grid = document.querySelector("#listings");
+    var bigHtmlString = _.template(template, listing);
+
+    grid.innerHTML = bigHtmlString;
+}
+
+EtsyClient.prototype.setupRouting = function() {
+    var self = this;
+
+    Path.map("#/").to(function() {
+        $.when(
+            self.loadTemplate("listing"),
+            self.pullAllActiveListings()
+        ).then(function() {
+            self.drawListings(arguments[0], arguments[1]);
+
+            console.dir(self)
+        })
+    });
+
+    Path.map("#/message/:anymessage").to(function() {
+        alert(this.params.anymessage);
+    })
+
+    Path.map("#/listing/:id").to(function() {
+        $.when(
+            self.loadTemplate("single-page-listing"),
+            self.pullSingleListing(this.params.id)
+        ).then(function() {
+            self.drawSingleListing(arguments[0], arguments[1]);
+        })
+    });
+
+    // set the default hash to draw all listings
+    Path.root("#/");
+    Path.listen();
+}
+
 
 /*
 var api_key = "cl15l8v7ye4vxkqzypp5pyk8";
